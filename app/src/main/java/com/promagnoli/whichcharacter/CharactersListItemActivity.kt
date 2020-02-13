8cea0650a350d3f53dffa0a4c5206c8955c08141
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2020-02-13 17:25:18 -0300

    Views and activities

---
+++ app/src/main/java/com/promagnoli/whichcharacter/CharactersListItemActivity.kt
@@ -0,0 +1,12 @@
+package com.promagnoli.whichcharacter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharactersListItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_characters_list_item)
+    }
+}
---
+++ app/src/main/java/com/promagnoli/whichcharacter/RandomCharacterActivity.kt
@@ -0,0 +1,12 @@
+package com.promagnoli.whichcharacter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomCharacterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_character)
+    }
+}
